3892699d42fb850e5e12d1c94dbf2d1e48fa2cc9
Oskari Noppa <the.nobba@gmail.com>
2018-07-01 04:49:39 +0300

    Create a simple test & fix ts config

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-tsc --target es5 --module umd --outDir .
-tsc --outDir es
+tsc --target es5 --module umd --outDir . index.ts
+tsc --target es6 --module es6 --outDir es index.ts
 cp es/index.js es/index.mjs
-tsc -d
+tsc -d index.ts
 cp index.d.ts es/
 cp index.js.flow es/
 cp index.js.flow es/index.mjs.flow
--- tests/get.ts
+++ tests/get.ts
@@ -1,7 +1,14 @@
 import {get} from '../index';
 
 describe('get function', () => {
-	it('should run tests', () => {
-		expect(true).toBe(true);
+	it('should find value when its there', () => {
+		const value = {};
+		const obj = {
+			a: {
+				b: value,
+			},
+		};
+
+		expect(get(obj, 'a', 'b')).toBe(value);
 	});
 });
