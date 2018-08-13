f0f42becc3fce7164717dbd1df1852ce312862b4
Oskari Noppa <oskari.noppa@bolt.works>
2018-08-13 16:12:27 +0300

    Fix tests

--- tests/typings/flow-typecheck.spec.ts
+++ tests/typings/flow-typecheck.spec.ts
@@ -5,32 +5,35 @@ import * as fs from 'fs';
 const executableExtension = process.platform === 'win32' ? '.cmd' : '';
 const testbed = path.join(__dirname, 'testbed');
 
-const checkFlow = (testdir: string): Promise<string> =>
-	new Promise((resolve, reject) => execFile(
-		path.join(ROOT_DIR, 'node_modules/.bin/flow' + executableExtension),
-		`check ${testdir} --all`.split(' '),
-		{
-			cwd: path.join(testbed),
-		},
-		(error, stdout, stderr) => {
-			if (error) {
-				reject(stdout || stderr || error);
-			} else {
-				resolve(stdout);
-			}
-		},
-	));
+const checkFlow = (testdir: string): Promise<string> => new Promise((resolve, reject) => {
+	try {
+		execFile(
+			path.join(ROOT_DIR, 'node_modules/.bin/flow' + executableExtension),
+			`check ${testdir} --all`.split(' '),
+			{
+				cwd: path.join(testbed),
+			},
+			(error, stdout, stderr) => {
+				if (error) {
+					reject(stdout || stderr || error);
+				} else {
+					resolve(stdout);
+				}
+			},
+		);
+	} catch (err) {
+		reject(err);
+	}
+});
 
-describe('Correct usage from Flow', () => {
-	it('should typecheck', () => {
+describe('Flow type definitions', () => {
+	it('should typecheck for correct usage', () => {
 		expect.assertions(1);
 		const result = checkFlow('flow-should-pass').then(_ => _.trim());
 		return expect(result).resolves.toBe('Found 0 errors');
 	});
-});
 
-describe('Incorrect usage from Flow', () => {
-	it('should not typecheck', () => {
+	it('should not typecheck for incorrect usage', () => {
 		expect.assertions(1);
 		const result = checkFlow('flow-should-not-pass');
 		return expect(result).rejects.toMatchSnapshot();
--- tests/typings/typescript-typecheck.spec.ts
+++ tests/typings/typescript-typecheck.spec.ts
@@ -1,34 +1,36 @@
-import {execFile} from 'child_process';
+import { execFile } from 'child_process';
 import * as path from 'path';
 
 const executableExtension = process.platform === 'win32' ? '.cmd' : '';
 
-const checkTs = (testdir: string) => new Promise((resolve, reject) => execFile(
-	path.join(ROOT_DIR, 'node_modules/.bin/tsc' + executableExtension),
-	`--noEmit --project ./ts-${testdir}/tsconfig.json`.split(' '),
-	{
-		cwd: path.join(__dirname, 'testbed'),
-	},
-	(error, stdout, stderr) => {
-		console.info('woop woop', error, stdout, stderr);
-		if (error) {
-			reject(stdout || stderr || error);
-		} else {
-			resolve(stdout);
-		}
-	},
-));
+const checkTs = (testdir: string) => new Promise((resolve, reject) => {
+	try {
+		execFile(
+			path.join(ROOT_DIR, 'node_modules/.bin/tsc' + executableExtension),
+			`--noEmit --project ./ts-${testdir}/tsconfig.json`.split(' '),
+			{
+				cwd: path.join(__dirname, 'testbed'),
+			},
+			(error, stdout, stderr) => {
+				if (error) {
+					reject(stdout || stderr || error);
+				} else {
+					resolve(stdout);
+				}
+			},
+		);
+	} catch (err) {
+		reject(err);
+	}
+});
 
-describe('Correct usage from TS', () => {
-	it('should typecheck', () => {
-		expect.assertions(1);
+describe('Typescript type definitions', () => {
+	it('should typecheck for correct usage', () => {
 		const result = checkTs('should-pass');
 		return expect(result).resolves.toBe('');
 	});
-});
 
-describe('Incorrect usage from TS', () => {
-	it('should not typecheck', () => {
+	it('should not typecheck for incorrect usage', () => {
 		expect.assertions(1);
 		const result = checkTs('should-not-pass');
 		return expect(result).rejects.toMatchSnapshot();
