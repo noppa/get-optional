a5a41d9484b1645db60873a4b85adc050a8b8153
Oskari Noppa <the.nobba@gmail.com>
2018-08-27 23:33:18 +0300

    Add getWithDefault tests for Flow types

---
+++ tests/typings/testbed/flow-should-not-pass/getWithDefault.js
@@ -0,0 +1,11 @@
+// @flow
+import {getWithDefault} from 'safeget';
+import {input} from '../flow-interfaces';
+import type {A, B, C, D, E} from '../flow-interfaces';
+
+// Error, because the result can also be bDefault
+const c: C = getWithDefault(bDefault, input, 'a', 'b', 'c');
+
+declare var bDefault: B;
+// Error, because property "c" is not in "a"
+const b = getWithDefault(bDefault, input, 'a', 'c');
--- tests/typings/testbed/flow-should-pass/getWithDefault.js
+++ tests/typings/testbed/flow-should-pass/getWithDefault.js
@@ -1,17 +1,15 @@
 // @flow
-// import {getWithDefault} from 'safeget';
-// import {A, B, C, D, E, input} from '../flow-interfaces.js';
+import {getWithDefault} from 'safeget';
+import {input} from '../flow-interfaces';
+import type {A, B, C, D, E} from '../flow-interfaces';
 
-// interface SomeType {
-// 	_kind: string;
-// }
+interface SomeType {
+	_kind: string;
+}
 
-// // Basic usage for all depths
-// const a: null | A = getWithDefault(null, input, 'a');
-// const b: number | B = getWithDefault(5, input, 'a', 'b');
-// const c: 'test' | C = getWithDefault('test', input, 'a', 'b', 'c');
-// const d: SomeType | D = getWithDefault({ _kind: 'foobar' }, input, 'a', 'b', 'c', 'd');
-// const e: E = getWithDefault(42, input, 'a', 'b', 'c', 'd', 'e');
-
-// const toFixed: void | typeof Number.prototype.toFixed = getWithDefault(undefined, e, 'toFixed');
-
+// Basic usage for all depths
+const a: null | A = getWithDefault(null, input, 'a');
+const b: number | B = getWithDefault(5, input, 'a', 'b');
+const c: 'test' | C = getWithDefault('test', input, 'a', 'b', 'c');
+const d: SomeType | D = getWithDefault({ _kind: 'foobar' }, input, 'a', 'b', 'c', 'd');
+const e: E = getWithDefault(42, input, 'a', 'b', 'c', 'd', 'e');
