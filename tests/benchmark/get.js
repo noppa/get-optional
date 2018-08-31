aca1e7b5ea123448deb5b933279876b3c571747f
Oskari Noppa <the.nobba@gmail.com>
2018-08-31 18:40:47 +0300

    Add lodash to benchmarks

--- tests/benchmark/get.js
+++ tests/benchmark/get.js
@@ -1,9 +1,12 @@
-const {Suite} = require('benchmark');
-// @ts-ignore
-global.currentImplementation = require('../../lib/index.js').get;
+const Benchmark = require('benchmark');
+const {Suite} = Benchmark;
+
+global.get$v1 = require('../../lib/index.js').get;
+global.get$lodash = require('lodash/get');
 
 function setup() {
-	const simpleInput = {a: 5};
+	let result$simple, result$complex;
+	const simpleInput = {a: 42};
 	const complexInput = {
 		a: {
 			b: {
@@ -19,19 +22,28 @@ function setup() {
 	}
 }
 
-function test() {
-	// @ts-ignore
-	currentImplementation({a: 5}, 'a');
-	// @ts-ignore
-	currentImplementation(complexInput, 'a', 'b', 'c', 'result');
+function v1Test() {
+	result$simple = get$v1(simpleInput, 'a');
+	result$complex = get$v1(complexInput, 'a', 'b', 'c', 'result');
+}
+
+function lodashTest() {
+	result$simple = get$lodash(simpleInput, ['a']);
+	result$complex = get$lodash(complexInput, ['a', 'b', 'c', 'result']);
+}
+
+function teardown() {
+	console.assert(result$simple === 42, 'Test failed with simple input, got ' + result$simple);
+	console.assert(result$complex === 'foobar', 'Test failed with complex input, got ' + JSON.stringify(result$complex));
 }
 
 const suite = new Suite('get');
 
 suite
-	.add('Current implementation', test, {setup})
-	.on('error', function(err) {
-		console.error(err);
+	.add('version 1', v1Test, { setup, teardown, })
+	.add('lodash', lodashTest, { setup, teardown, })
+	.on('error', function({target}) {
+		console.error(`Benchmark for ${target.name} failed with error `, target.error);
 	})
 	.on('complete', function() {
 		console.log(this.map(_ => _.toString()).join('\n'))
