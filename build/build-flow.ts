8fac43dc8210dfb61955812b13c0a79c393b03c5
Oskari Noppa <the.nobba@gmail.com>
2018-09-05 00:07:41 +0300

    Gnerate Flow types for method fn
    Also line changes to Flow test snapshots.

--- build/build-flow.ts
+++ build/build-flow.ts
@@ -3,9 +3,14 @@ import { getter, buildWith } from './generators';
 
 const notNil = (type: string) => `$NonMaybeType<${type}>`;
 
-function* generatorForGetters(tabs: TabsProvider, withDefaultValue: boolean) {
+function* generatorForGetters(
+	tabs: TabsProvider,
+	withDefaultValue: boolean,
+	forMethod: boolean,
+) {
 	yield* getter(tabs, {
 		withDefaultValue,
+		forMethod,
 		notNil,
 		prevIndexer(prevIndex, prevType) {
 			return prevIndex > 0
@@ -18,19 +23,28 @@ function* generatorForGetters(tabs: TabsProvider, withDefaultValue: boolean) {
 				: 'Prop';
 			return `Key${keyNumber}: ${typeRestriction}`;
 		},
-		returnType(keyNumber, prevType, defaultType) {
-			let successResult = `$ElementType<${prevType}, Key${keyNumber}>`;
-			if (defaultType) {
-				successResult = notNil(successResult);
-			} else {
-				defaultType = 'void';
-			}
-			return `: ${defaultType} | ` + successResult;
-		},
+		returnType: forMethod ? returnTypeForMethodGetter : returnTypeForGetter,
 		exportVar(varname, typename) {
 			return `declare export var ${varname}: ${typename}`;
 		},
 	});
+}
+
+function returnTypeForMethodGetter(keyNumber: number, prevType: string): string {
+	// The method function either returns the item in path (which should be a function)
+	// or a "noop" function.
+	const noopType = `((...args: any[]) => void)`;
+	return returnTypeForGetter(keyNumber, prevType, noopType);
+}
+
+function returnTypeForGetter(keyNumber: number, prevType: string, defaultType?: string): string {
+	let successResult = `$ElementType<${prevType}, Key${keyNumber}>`;
+	if (defaultType) {
+		successResult = notNil(successResult);
+	} else {
+		defaultType = 'void';
+	}
+	return `: ${defaultType} | ` + successResult;
 }
 
 function* generatorForNth() {
@@ -48,19 +62,24 @@ function* generatorForNth() {
 
 export default function buildFlow() {
 	const tabs = new TabsProvider();
+	const br = '\n';
+
 	const fileStart = [
 		'// @flow',
 		'type Prop = string | number;',
 		'',
-	].join('\n');
-
+	];
 	const generatedGetters = [false, true]
-		.map(withDefaultValue => buildWith(generatorForGetters(tabs, withDefaultValue), tabs))
-		.join('\n')
-		+ '\n';
-
+		.map(withDefaultValue => buildWith(generatorForGetters(tabs, withDefaultValue, false), tabs))
 	const generatedNth = buildWith(generatorForNth(), tabs);
+	const generatedMethodGetter = buildWith(generatorForGetters(tabs, false, true), tabs);
 
-	const result = fileStart + generatedGetters + generatedNth;
+	const result = [
+		...fileStart,
+		...generatedGetters,
+		generatedNth,
+		generatedMethodGetter,
+	].join(br);
+
 	return writeFile(relativeToRoot('lib', 'index.js.flow'), result);
 }
