493d2a956000851b9a6ca4d4b94c0c31fbca4a21
Oskari Noppa <the.nobba@gmail.com>
2018-09-05 02:55:07 +0300

    Rename couple of internal variables

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,39 @@
 
 // Base implementation for all getters.
-// Gets the value in the given path and calls `fn` with it
+// Gets the value in the given path and calls `interceptor` with it
 // and the previous context object or returns undefined if the path is not found.
-function mapNullable(fn: Function, object, key1, key2, key3, key4, key5) {
+function mapProperty(interceptor: Function, object, key1, key2, key3, key4, key5) {
 	let value1, value2, value3, value4;
 	if (object == null)     return undefined;
 	
 	value1 = object[key1];
-	if (key2 === undefined) return fn(value1, object);
+	if (key2 === undefined) return interceptor(value1, object);
 	if (value1 == null)     return undefined;
 
 	value2 = value1[key2];
-	if (key3 === undefined) return fn(value2, value1);
+	if (key3 === undefined) return interceptor(value2, value1);
 	if (value2 == null)     return undefined;
 
 	value3 = value2[key3];
-	if (key4 === undefined) return fn(value3, value2);
+	if (key4 === undefined) return interceptor(value3, value2);
 	if (value3 == null)     return undefined;
 
 	value4 = value3[key4];
-	if (key5 === undefined) return fn(value4, value3);
+	if (key5 === undefined) return interceptor(value4, value3);
 	if (value4 == null)     return undefined;
 
-	return fn(value4[key5], value4);
+	return interceptor(value4[key5], value4);
 }
 
 function identity(a) { return a; }
 
 function getWithDefault (defaultValue, obj, key1, key2, key3, key4, key5) {
-	const result = mapNullable(identity, obj, key1, key2, key3, key4, key5);
+	const result = mapProperty(identity, obj, key1, key2, key3, key4, key5);
 	return result == null ? defaultValue : result;
 }
 
 function get(obj, key1, key2, key3, key4, key5) {
-	return mapNullable(identity, obj, key1, key2, key3, key4, key5);
+	return mapProperty(identity, obj, key1, key2, key3, key4, key5);
 }
 
 function noop() {} // tslint:disable-line:no-empty
@@ -47,7 +47,7 @@ function bind(fn, context) {
 }
 
 function method(obj, key1, key2, key3, key4, key5) {
-	const result = mapNullable(bind, obj, key1, key2, key3, key4, key5);
+	const result = mapProperty(bind, obj, key1, key2, key3, key4, key5);
 	return result == null ? noop : result;
 }
 
